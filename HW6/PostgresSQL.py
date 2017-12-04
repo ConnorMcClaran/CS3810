2fe650e94bd4d2ed6ff37fa893dfcbdaf8e02bf6
ConnorMcClaran <cmcclaran62@gmail.com>
2017-12-04 15:14:07 -0700

    Add files via upload

--- HW6/PostgresSQL.py
+++ HW6/PostgresSQL.py
@@ -11,7 +11,20 @@ Area_code = input('Phone Ares Code(3 digits):')
 Local_number = input('Remaining Phone digits:')
 Email = input('Email:')
 
-#Create customer
+try:
+    a = int(Area_code)
+    b = int(Local_number)
+except ValueError:
+
+    print('Invalid phone entered')
+
+try:
+    if '@' in Email :
+        print('Email accepted')
+except ValueError:
+     print('Invalid Email entered')
+    
+#Create customer in database
 def input_customer(First_name,Last_name,Street,City,Country,Area_code,Local_number,Email):
     sql = """INSERT INTO Customers (First_name,Last_name,Street,City,Country,Area_code,Local_number,Email1)
          VALUES (%s);"""
@@ -31,7 +44,7 @@ def input_customer(First_name,Last_name,Street,City,Country,Area_code,Local_numb
     
         cur.close()
     except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+        print('No connection')
 
     finally:
         if conn is not None:
@@ -40,4 +53,125 @@ def input_customer(First_name,Last_name,Street,City,Country,Area_code,Local_numb
     return Customer_id
 
 input_customer(First_name,Last_name,Street,City,Country,Area_code,Local_number,Email)
+
+TorontoDest = ['New York','Chicago','London','Montreal','Edinburgh']
+NewYorkDest = ['Chicago','Toronto']
+ChicagoDest = ['New York','Toronto']
+LondonDest = ['Toronto','Edinburgh','Paris','Nice']
+EdinburghDest = ['Toronto','Montreal','London','Paris','Nice']
+ParisDest = ['London','Edinburgh','Nice']
+NiceDest = ['London','Edinburgh','Paris']
+MontrealDest = ['Toronto','London','Edinburgh']
+
+               
+Origin = input('What is your origin City:')
+attempts  = 0 
+while attempts < 3:
+    if Origin == 'Toronto' :
+        print('Avaiable Destinations:',TorontoDest)
+        dest = input('What is your Destination:')
+
+        try:
+            if dest in TorontoDest:
+                print('Destination Accepted')
+                break
+        except:
+            print('Invalid City Select from avaiable destinations')
+            attempts = attempts + 1
+
+    if Origin == 'New York' :
+        print('Avaiable Destinations:',NewYorkDest)
+        dest = input('What is your Destination:')
+
+        try:
+            if dest in NewYorkDest:
+                print('Destination Accepted')
+                break
+        except:
+            print('Invalid City Select from avaiable destinations')
+            attempts = attempts + 1
+
+    if Origin == 'Chicago' :
+        print('Avaiable Destinations:',ChicagoDest)
+        dest = input('What is your Destination:')
+
+        try:
+            if dest in ChicagoDest:
+                print('Destination Accepted')
+                break
+        except:
+            print('Invalid City Select from avaiable destinations')
+            attempts = attempts + 1
+
+    if Origin == 'London' :
+        print('Avaiable Destinations:',LondonDest)
+        dest = input('What is your Destination:')
+
+        try:
+            if dest in LondonDest:
+                print('Destination Accepted')
+                break
+        except:
+            print('Invalid City Select from avaiable destinations')
+            attempts = attempts + 1
+            
+    if Origin == 'Edinburgh' :
+        print('Avaiable Destinations:',EdinburghDest)
+        dest = input('What is your Destination:')
+
+        try:
+            if dest in EdinburghDest:
+                print('Destination Accepted')
+                break
+        except:
+            print('Invalid City Select from avaiable destinations')
+            attempts = attempts + 1
+            
+    if Origin == 'Paris' :
+        print('Avaiable Destinations:',ParisDest)
+        dest = input('What is your Destination:')
+
+        try:
+            if dest in ParisDest:
+                print('Destination Accepted')
+                break
+        except:
+            print('Invalid City Select from avaiable destinations')
+            attempts = attempts + 1
+            
+    if Origin == 'Nice' :
+        print('Avaiable Destinations:',NiceDest)
+        dest = input('What is your Destination:')
+
+        try:
+            if dest in NiceDest:
+                print('Destination Accepted')
+                break
+        except:
+            print('Invalid City Select from avaiable destinations')
+            attempts = attempts + 1
+            
+    if Origin == 'Montreal' :
+        print('Avaiable Destinations:',MontrealDest)
+        dest = input('What is your Destination:')
+
+        try:
+            if dest in MontrealDest:
+                print('Destination Accepted')
+                break
+        except:
+            print('Invalid City Select from avaiable destinations')
+            attempts = attempts + 1
+            
+
+            
+
+            
+    
+    
+    
+          
+
+
+
 
